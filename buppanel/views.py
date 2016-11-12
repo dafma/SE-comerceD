7934f4d78d31180a94a5ea6f9ad641428e1898ec
dafma <mar_tp@hotmail.es>
2016-11-11 19:02:02 -0600

    agregado lista con  cards falat redireccionar producto

--- buppanel/views.py
+++ buppanel/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from shop.models import Producto, Categoria
 
 # Create your views here.
 
 def panel(request):
-	return render(request,'panel_principal.html', )
+	productos = Producto.objects.all()
+	categorias = Categoria.objects.all()
+	return render(request,'panel_principal.html', 
+												{'productos': productos,
+												  'categorias':categorias})
